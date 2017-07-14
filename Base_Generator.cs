84c4fb1132e2ad46664d5f3f1fd1c1652142cd56
Jurasikll <Jurasikll@yandex.ru>
2017-07-14 16:57:37 +0300

    0.0.3
    Gettters

--- Base_Generator.cs
+++ Base_Generator.cs
@@ -8,5 +8,50 @@ namespace Generator
 {
     class Base_Generator
     {
+        private UInt32 width;
+        private UInt32 height;
+        private UInt32 levelsCount;
+        List<byte> levelsFillRatio;
+
+        public Boolean set_map_params(UInt32 width, UInt32 height, UInt32 levelsCount, List<byte> levelsFillRatio)
+        {
+            Int32 countLevelsFillRatio = levelsFillRatio.Count;
+            Boolean res = false;
+            const string ERROR_MSG_UNCORECT_LEVELS_COUNT = "Неверное колличество уровней карты (levelsCount и levelsFillRatio.count должны быть равны)";
+
+            if (levelsCount == countLevelsFillRatio)
+            {
+                this.width = width;
+                this.height = height;
+                this.levelsCount = levelsCount;
+                this.levelsFillRatio = new List<byte>(countLevelsFillRatio);
+                for (int i = 0; i < levelsCount; i++)
+                {
+                    this.levelsFillRatio.Add(levelsFillRatio[i]);
+                }
+                res = true;
+
+            } else
+            {
+                throw new Exception(ERROR_MSG_UNCORECT_LEVELS_COUNT);
+            }
+            return res;
+        }
+        public UInt32 Width 
+        {
+            get { return this.width; }
+        }
+        public UInt32 Height
+        {
+            get { return this.height; }
+        }
+        public UInt32 LevelsCount
+        {
+            get { return this.levelsCount; }
+        }
+        public List<byte> LevelsFillRatio
+        {
+            get { return this.levelsFillRatio; }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,9 +10,13 @@ namespace Generator
     {
         static void Main(string[] args)
         {
-            int test;
-            test = 1 + 1;
-            Console.WriteLine(test);
+            Base_Generator gen = new Base_Generator();
+            List<byte> testFR = new List<byte>() { 45,31,27,42,87,26,93,58,62,29};
+            gen.set_map_params(10, 10, 10, testFR);
+            foreach(byte i in gen.LevelsFillRatio)
+            {
+                Console.WriteLine(i);
+            }
             Console.ReadKey();
         }
     }
